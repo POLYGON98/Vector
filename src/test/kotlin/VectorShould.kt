72b0bcffedd344ce2fa25d044e1d7f283985a3c9
matthewp <matthew.pollard@mkodo.com>
2020-08-20 11:57:24 +0100

    test refactor

--- src/test/kotlin/VectorShould.kt
+++ src/test/kotlin/VectorShould.kt
@@ -1,30 +1,35 @@
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class VectorShould {
+    private lateinit var vectorA: Vector
+    private lateinit var vectorB: Vector
+
+    @BeforeEach
+    internal fun setUp() {
+        vectorA = Vector(1, 2, 3)
+        vectorB = Vector(arrayOf(1, 2, 3))
+    }
 
     @Test
     internal fun `return string representation of 3d vector given three parameters`() {
-        val vector = Vector(1, 2, 3)
-        assertEquals("<1, 2, 3>", vector.toS())
+        assertEquals("<1, 2, 3>", vectorA.toS())
     }
 
     @Test
     internal fun `return string representation of 3d vector given array parameter`() {
-        val vector = Vector(arrayOf(1, 2, 3))
-        assertEquals("<1, 2, 3>", vector.toS())
+        assertEquals("<1, 2, 3>", vectorB.toS())
     }
 
     @Test
     internal fun `return array representation of 3d vector given three parameters`() {
-        val vector = Vector(1, 2, 3)
-        assertArrayEquals(arrayOf(1, 2, 3), vector.toA())
+        assertArrayEquals(arrayOf(1, 2, 3), vectorA.toA())
     }
 
     @Test
     internal fun `return array representation of 3d vector given array parameter`() {
-        val vector = Vector(arrayOf(1, 2, 3))
-        assertArrayEquals(arrayOf(1, 2, 3), vector.toA())
+        assertArrayEquals(arrayOf(1, 2, 3), vectorB.toA())
     }
 }
